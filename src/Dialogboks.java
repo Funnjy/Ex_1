c203f466ac14e3a279b51ae047faaa28e5b93bd1
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-03-29 22:11:09 +0200

    basic exercise 2

--- src/Dialogboks.java
+++ src/Dialogboks.java
@@ -1,10 +1,13 @@
 /**
  * Created by Anton Ilchenko on 29.03.2017.
  */
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.JOptionPane;
 public class Dialogboks {
     public static void main(String[] args){
-        exercise_1();
+        //exercise_1();
+        exercise_2();
     }
 
     public static void exercise_1(){
@@ -20,5 +23,29 @@ public class Dialogboks {
 
         sum = number+number2;
         JOptionPane.showMessageDialog(null, "Sum av " + number+" og "+number2+" = "+sum);
+    }
+
+    public static void exercise_2(){
+        double n1 = 0;
+        double n2 = 0;
+        double sum = 0;
+        double produkt = 0;
+        double modul = 0;
+
+        String nString;
+
+        nString = JOptionPane.showInputDialog("Skriv nummer 1");
+        n1 = Double.parseDouble(nString);
+
+        nString = JOptionPane.showInputDialog("Skriv nummer 2");
+        n2 = Double.parseDouble(nString);
+
+        sum = n1+n2;
+        produkt = n1*n2;
+        modul = n1%n2;
+
+        JOptionPane.showMessageDialog(null, "Sum: " + sum
+                                                                    + " \nProdukt " + produkt
+                                                                    + "\nModul " + modul);
     }
 }
