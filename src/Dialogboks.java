9539a7cf5bdbbd9dd9fef877e10a69bfa834b97a
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-03-29 21:59:15 +0200

    basic exercise 1 fix

--- src/Dialogboks.java
+++ src/Dialogboks.java
@@ -10,11 +10,15 @@ public class Dialogboks {
     public static void exercise_1(){
         int number = 0;
         int number2 = 0;
+        int sum = 0;
         String numberstring;
         numberstring = JOptionPane.showInputDialog("Print number 1");
         number = Integer.parseInt(numberstring);
+
         numberstring = JOptionPane.showInputDialog("Print number 1");
         number2 = Integer.parseInt(numberstring);
-        JOptionPane.showMessageDialog(null, number+number2);
+
+        sum = number+number2;
+        JOptionPane.showMessageDialog(null, "Sum av " + number+" og "+number2+" = "+sum);
     }
 }
