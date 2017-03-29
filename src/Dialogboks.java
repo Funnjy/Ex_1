55b80e81c5da9adc17d718f06231f951dc132f88
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-03-29 21:55:59 +0200

    basic exercise 1

--- src/Dialogboks.java
+++ src/Dialogboks.java
@@ -4,6 +4,17 @@
 import javax.swing.JOptionPane;
 public class Dialogboks {
     public static void main(String[] args){
-        JOptionPane.showMessageDialog(null,"Hi memory!");
+        exercise_1();
+    }
+
+    public static void exercise_1(){
+        int number = 0;
+        int number2 = 0;
+        String numberstring;
+        numberstring = JOptionPane.showInputDialog("Print number 1");
+        number = Integer.parseInt(numberstring);
+        numberstring = JOptionPane.showInputDialog("Print number 1");
+        number2 = Integer.parseInt(numberstring);
+        JOptionPane.showMessageDialog(null, number+number2);
     }
 }
