aa7499b74f426cf225e2ae530fb0d889c771f676
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-03-29 19:34:33 +0200

    Initial commit

---
+++ src/Dialogboks.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Anton Ilchenko on 29.03.2017.
+ */
+import javax.swing.JOptionPane;
+public class Dialogboks {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Hi memory!");
+    }
+}
