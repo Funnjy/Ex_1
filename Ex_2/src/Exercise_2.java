35d6ae96598ddea41098ae52c4de6debf62dac37
Anton <ilchenko.anton@gmail.com>
2017-03-30 21:04:11 +0200

    Coursbook exercise, working on

--- Ex_2/src/Exercise_2.java
+++ Ex_2/src/Exercise_2.java
@@ -1,5 +1,12 @@
+import sun.plugin2.message.JavaObjectOpMessage;
+
+import javax.swing.*;
+
 /**
  * Created by Anton Ilchenko on 30.03.2017.
  */
 public class Exercise_2 {
+    public void visTittel(String kursnavn){
+        JOptionPane.showMessageDialog(null, "Kursnavn er " + kursnavn);
+    }
 }
---
+++ Ex_2/src/Exercise_2test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Anton on 30.03.2017.
+ */
+public class Exercise_2test{
+    public static void main(String[] args) {
+        Exercise_2 bok = new Exercise_2();
+        bok.visTittel("ADATE");
+    }
+}
