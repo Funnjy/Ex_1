ef690542a6e9b5f030c00d65490e57e22efb1ff2
Anton <ilchenko.anton@gmail.com>
2017-04-03 16:26:27 +0200

    Added Veksleautomat methods. Upgrade and make it possible to use Double values

--- Veksleautomat/src/Veksleautomat.java
+++ Veksleautomat/src/Veksleautomat.java
@@ -1,6 +1,39 @@
+import javax.swing.*;
+
 /**
  * Created by Anton on 03.04.2017.
  */
 public class Veksleautomat {
+    private int max;
+    private int f_lapp, t_lapp, e_lapp;
+    private int belop;
 
+    public Veksleautomat(){
+        max = 100;
+        f_lapp = 0;
+        t_lapp = 0;
+        e_lapp = 0;
+        belop = 0;
+    }
+
+    public void calc_ny_max(int n){ //Calculer max after payd belop
+        if(max-n >= 0)
+            max -= n;
+        else JOptionPane.showMessageDialog(null, "Beløp må være mindre enn 100!");
+    }
+
+    public void veksling_resultat(){
+        f_lapp = max/50;
+        max = max%50;
+        t_lapp = max/20;
+        max = max%20;
+        e_lapp = max/1;
+    }
+
+    public void resultat(){
+        JOptionPane.showMessageDialog(null, "Tilbake:\n" +
+                f_lapp + " 50-lapp\n" +
+                t_lapp + " 20-lapp\n" +
+                e_lapp + " 1-lapp");
+    }
 }
---
+++ Veksleautomat/src/Veksleautomattest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Anton on 03.04.2017.
+ */
+public class Veksleautomattest {
+    public static void main(String[] args){
+        Veksleautomat veks = new Veksleautomat();
+
+        veks.calc_ny_max(100);
+        veks.veksling_resultat();
+        veks.resultat();
+    }
+}
