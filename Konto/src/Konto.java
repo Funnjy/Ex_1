16fda132d5fa64b68a70d01b3ca9fe4763b1acf0
Anton <ilchenko.anton@gmail.com>
2017-04-03 15:22:06 +0200

    Finished Konto methods Now building Kontotest

--- Konto/src/Konto.java
+++ Konto/src/Konto.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created by Anton on 03.04.2017.
  */
@@ -35,21 +37,35 @@ public class Konto {
     }
 
     //<  Kontoutskrifts-metode, som skriver ut kontoinnehaverens
-    public void konto_out(){
-
-    }
     //navn, kontonummer og saldo i et dialogvindu. >
+    public void konto_out(){
+        JOptionPane.showMessageDialog(null, "Brukeren av konto: " + get_name() +
+                                    "\nKontonummer er: " + get_k_number() +
+                                    "\nSaldo er: " + get_saldo());
+    }
 
     //<  Metode som setter inn et beløp på kontoen.
     //Beløpets størrelse skal tas imot via en parameter til metoden.
     //Metoden skal returnere en tekst som inneholder informasjon om
     //den nye saldoen.  >
-
+    public void til_konto(int n){
+        saldo += n;
+        JOptionPane.showMessageDialog(null, "Du har lagt til: "
+                + n + " , nåvaærende saldo er: " + saldo);
+    }
     //<  Metode som tar ut et beløp fra kontoen, under forutsetning av
     //at det er dekning for beløpet.
     //        Uttaks-beløpets størrelse skal tas imot via en parameter til metoden.
     //Hvis det er dekning på kontoen, skal metoden returnere en tekst
     //med informasjon om den nye saldoen. Hvis  det ikke er dekning,
     //skal den returnerte teksten inneholde informasjon om det. >
+    public void fra_konto(int n){
+        if(saldo-n >= 0 ){
+            saldo -= n;
+            JOptionPane.showMessageDialog(null, "Du har tat ut: "
+                    + n + " , ny saldo er: " +saldo);
+        }
+        else JOptionPane.showMessageDialog(null, "Det er ikke nåk penger på kontoen din.");
+    }
 
 }
