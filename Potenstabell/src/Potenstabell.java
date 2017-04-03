e1480cfdff78b829fb28e4eee9de233b94683476
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-04-04 00:04:32 +0200

    Potenstabell update

--- Potenstabell/src/Potenstabell.java
+++ Potenstabell/src/Potenstabell.java
@@ -1,5 +1,12 @@
 /**
  * Created by Anton Ilchenko on 03.04.2017.
  */
+
+import javax.swing.JTextArea;
 public class Potenstabell {
+    int tall = (int) (Math.random() * 100);
+
+    private JTextArea textarea = new JTextArea();
+
+
 }
