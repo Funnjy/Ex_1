37e959fe87f5bbca64779a424fd7365f6e82cfd6
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-04-08 21:09:19 +0200

    Trying JTextArea

--- Potenstabell/src/Potenstabell.java
+++ Potenstabell/src/Potenstabell.java
@@ -3,10 +3,19 @@
  */
 
 import javax.swing.JTextArea;
+
 public class Potenstabell {
     int tall = (int) (Math.random() * 100);
 
     private JTextArea textarea = new JTextArea();
+
+    public void set_Text(String text){
+        textarea.setText(text);
+    }
+
+    public JTextArea get_textarea(){
+        return textarea;
+    }
 
 
 }
