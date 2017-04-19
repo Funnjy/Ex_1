d3581fd3682adf62fa6ef2e96b335bb52bab4cab
Anton <ilchenko.anton@gmail.com>
2017-04-19 10:34:25 +0200

    Potenstabell shows 100 numbers between 0 and 100 and shows biggest, next biggest, min and next min

--- Potenstabell/src/Potenstabell.java
+++ Potenstabell/src/Potenstabell.java
@@ -6,7 +6,9 @@ import javax.swing.JTextArea;
 public class Potenstabell {
     int tall = (int) (Math.random() * 100);
 
-    private JTextArea textarea = new JTextArea();
+    //Create JTextArea
+    public JTextArea textarea = new JTextArea();
+
 
     public void set_text(String text_in){
         textarea.setText(text_in);
@@ -15,5 +17,4 @@ public class Potenstabell {
     public JTextArea get_textarea(){
         return textarea;
     }
-
 }
--- Potenstabell/src/Potenstabelltest.java
+++ Potenstabell/src/Potenstabelltest.java
@@ -7,10 +7,49 @@ public class Potenstabelltest {
     public static void main(String[]args){
         int tall = (int) (Math.random() * 100);
         String h_input;
+        int m = 100, s = 0;
+        int nm = m, ns = s;
+        //Create new Potenstabell
         Potenstabell ptabell = new Potenstabell();
-        h_input = JOptionPane.showInputDialog(null, "Write your text");
-        ptabell.set_text(h_input);
+
+        //Toptext for text area
+        ptabell.set_text("Tall\n");
+
+        for(int i = 1; i < 100; i++){
+            tall = (int) (Math.random() * 100);
+            if(m == -1) m = tall;
+            if(s == -1) s = tall;
+
+            if(i%10 != 0 /*|| i == 0*/) {
+                ptabell.textarea.append(tall + "\t");
+            }
+            else{
+                ptabell.textarea.append(tall +"\n");
+            }
+
+            if(tall < m ){
+                nm = m;
+                m = tall;
+            }else if( tall < nm && tall > m) nm = tall;
+
+            if(tall > s){
+                ns = s;
+                s = tall;
+            }else if(tall > ns && tall < s) ns = tall;
+        }
+
+        ptabell.textarea.append("\n\nMinst: " + m
+                + ", Nest Minst: " + nm
+                + ", Størst: " + s
+                + ", Nest Størst: " + ns);
+        //h_input = JOptionPane.showInputDialog(null, "Write your text");
+        //ptabell.set_text(h_input);
+
+        //Show table of all numbers
         JOptionPane.showMessageDialog(null, ptabell.get_textarea(), "Testing JText Area",
                 JOptionPane.INFORMATION_MESSAGE);
+
+
     }
+
 }
