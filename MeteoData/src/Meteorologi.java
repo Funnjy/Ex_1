e91afe26ac8ee501c7f49ad0abf63ec7627ff513
Anton <ilchenko.anton@gmail.com>
2017-04-20 09:42:37 +0200

    MeteoData, work on Statistikk class methods

---
+++ MeteoData/src/Meteorologi.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Anton on 20.04.2017.
+ */
+public class Meteorologi {
+}
---
+++ MeteoData/src/Statistikk.java
@@ -0,0 +1,59 @@
+/**
+ * Created by Anton on 20.04.2017.
+ */
+public class Statistikk {
+
+    //Variables definition
+    private String måned;
+    private int år;
+    private int[] minTemp;
+    private int[] maxTemp;
+    private int[] nedbør;
+
+    //Constructor
+    public Statistikk(String mnd, int å, int[] min, int[] max, int[] ned){
+        måned = mnd;
+        år = å;
+        minTemp = min;
+        maxTemp = max;
+        nedbør = ned;
+    }
+
+    //Rainy days that have more then 5mm
+    public int regndager(){
+
+        int teller = 0;
+
+        for(int i = 0; i < nedbør.length; i ++){
+            if(nedbør[i] > 5){
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    //Median temperature of days with more than 5 mm rain
+    public double maksTempSnitt(){
+        double sumTemp = 0;
+        double teller = regndager();
+
+        if(teller == 0){
+            return -100.00; //If no such days found
+        }
+
+        for(int i = 0; i < maxTemp.length; i++){
+            if(nedbør[i] > 5){
+                sumTemp += maxTemp[i];
+            }
+        }
+        return sumTemp/teller;
+    }
+
+    //Return biggest temperature variation for one day of the month
+    public int maksTempVariasjon(){
+
+    }
+
+
+
+}
