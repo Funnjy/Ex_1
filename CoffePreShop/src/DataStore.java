0a4b9062ea91de4df28e5fab2a3b8132b1c09258
Anton <ilchenko.anton@gmail.com>
2017-06-04 00:22:40 +0200

    After exam commit

---
+++ CoffePreShop/src/DataStore.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Anton on 03.06.2017.
+ */
+public class DataStore {
+    int [] temp_array = {1,2,3,4,5,6};
+
+    public void show_items(JTextArea text){
+
+    }
+}
