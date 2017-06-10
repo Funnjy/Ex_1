7447c87a6f9562789639ad36b2909988235b7573
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-06-11 00:59:20 +0200

    FileStreamT class w. comm

--- Files/src/FileStreamT.java
+++ Files/src/FileStreamT.java
@@ -8,14 +8,14 @@ public class FileStreamT {
     public static void main (String args[]) {
         try{
             byte bWrite[] = {10,21,3,40,5};
-
+            //Write to the file
             OutputStream os = new FileOutputStream("test.txt");
 
             for (int i = 0; i < bWrite.length; i++){
                 os.write(bWrite[i]);
             }
             os.close();
-
+            //Output to the console
             InputStream is = new FileInputStream("test.txt");
             int size = is.available();
 
