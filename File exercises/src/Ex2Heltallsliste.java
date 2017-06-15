d2f4a57632c593416cf9b1debfe01c1753b5666c
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-06-16 00:18:57 +0200

    Ex2Node find() added

--- File exercises/src/Ex2Heltallsliste.java
+++ File exercises/src/Ex2Heltallsliste.java
@@ -4,11 +4,26 @@
 public class Ex2Heltallsliste {
 
     private Ex2Node first;
+
     public Ex2Heltallsliste(){
         first = null;
     }
 
     public Ex2Node find(int n){
+        Ex2Node runner = first;
+        while (first != null){
+
+            if(first.getInfo() == n){
+                return first;
+            }else{
+                runner = runner.neste;
+            }
+        }
+        return null;
+    }
+
+    public boolean remove(int n){
+        Ex2Node remove = find(n);
 
     }
 }
