1d3b9399b3ea9e5255adf46057d968e1324b44bc
Anton Ilchenko <anton.ilchenko@hiof.no>
2017-12-18 13:08:31 +0100

    Learning Threads
    Threads

---
+++ Counter/src/com/antonilchenko/Main.java
@@ -0,0 +1,50 @@
+package com.antonilchenko;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CountDown countDown = new CountDown();
+
+        CountDownThread t1 = new CountDownThread(countDown);
+        t1.setName("Thread 1");
+
+        CountDownThread t2 = new CountDownThread(countDown);
+        t2.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class CountDown {
+    public void doCountdown() {
+        String color;
+
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadsColors.ANSI_CYAN;
+                break;
+            case "Thread 2"
+                color = ThreadsColors.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadsColors.ANSI_GREEN;
+        }
+
+        for (int i = 10; i < 0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + ": i =" + i);
+        }
+    }
+}
+
+class CountDownThread extends Thread {
+    private CountDown threadCountdown;
+
+    public CountDownThread(CountDown countDown) {
+        threadCountdown = countDown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}
